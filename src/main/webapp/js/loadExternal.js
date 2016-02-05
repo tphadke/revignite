b5af2d8884a8308af391eff044bf4486052cf67d
tanujaPhadke <tanuja_phadke@intuit.com>
2016-02-04 16:06:06 -0800

    Project Start

---
+++ src/main/webapp/js/loadExternal.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('#about-content').load('../pages/about.html');
+	
+	$('#culture-content').load('../pages/culture.html');
+
+	$('#cope-content').load('../pages/cope.html');
+
+	
+	$('#men-content').load('../pages/men.html');
+
+	
+	$('#women-content').load('../pages/women.html');
+
+	
+	$('#about-content').load('../pages/cultureBuilding.html');
+	
+	$('#videos-content').load('../pages/videos.html');
+
+
+});
