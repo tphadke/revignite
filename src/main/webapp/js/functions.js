e90aa883e57d9d25452994702ea5136cda9b36ff
Pawansing Pardeshi <pawansing.pardeshi@mindtrickssoftware.com>
2016-02-12 15:16:18 +0530

    open popup

--- src/main/webapp/js/functions.js
+++ src/main/webapp/js/functions.js
@@ -3,3 +3,56 @@ function showFirstModal()
 
 function showSecondModal()
 { $('#Participate_in_group_therapy').modal('show'); }
+
+function showmenlink_one(){
+	$('#menlink_1').modal('show');
+}
+
+function showmenlink_two(){
+	$('#menlink_2').modal('show');
+}
+
+function showmenlink_three(){
+	$('#menlink_3').modal('show');
+}
+
+function showmenlink_four(){
+	$('#menlink_4').modal('show');
+}
+
+function showmenlink_five(){
+	$('#menlink_5').modal('show');
+}
+
+function showmenlink_six(){
+	$('#menlink_6').modal('show');
+}
+
+function showmenlink_seven(){
+	$('#menlink_7').modal('show');
+}
+
+function showmenlink_eight(){
+	$('#menlink_8').modal('show');
+}
+
+function showmenlink_nine(){
+	$('#menlink_9').modal('show');
+}
+
+function showmenlink_ten(){
+	$('#menlink_10').modal('show');
+}
+
+function showmenlink_eleven(){
+	$('#menlink_11').modal('show');
+}
+
+function showmenlink_twele(){
+	$('#menlink_12').modal('show');
+}
+
+function showmenlink_thirteen(){
+	$('#menlink_13').modal('show');
+}
+
--- src/main/webapp/js/loadExternal.js
+++ src/main/webapp/js/loadExternal.js
@@ -14,7 +14,48 @@ $(document).ready(function() {
 	});
 
 	
-	$('#men-content').load('../pages/men.html');
+	$('#men-content').load('../pages/men.html',function(){ 
+		$("#menlink_one").click(function(){ 
+			showmenlink_one();
+		});	
+		$("#menlink_two").click(function(){ 
+			showmenlink_two();
+		});	
+		$("#menlink_three").click(function(){ 
+			showmenlink_three();
+		});	
+		$("#menlink_four").click(function(){ 
+			showmenlink_four();
+		});	
+		$("#menlink_five").click(function(){ 
+			showmenlink_five();
+		});	
+		$("#menlink_six").click(function(){ 
+			showmenlink_six();
+		});	
+		$("#menlink_seven").click(function(){ 
+			showmenlink_seven();
+		});	
+		$("#menlink_eight").click(function(){ 
+			showmenlink_eight();
+		});	
+		$("#menlink_nine").click(function(){ 
+			showmenlink_nine();
+		});	
+		$("#menlink_ten").click(function(){ 
+			showmenlink_ten();
+		});	
+		$("#menlink_eleven").click(function(){ 
+			showmenlink_eleven();
+		});
+		$("#menlink_twele").click(function(){ 
+			showmenlink_twele();
+		});	
+		$("#menlink_thirteen").click(function(){ 
+			showmenlink_thirteen();
+		});	
+		
+	});
 
 	
 	$('#women-content').load('../pages/women.html');
